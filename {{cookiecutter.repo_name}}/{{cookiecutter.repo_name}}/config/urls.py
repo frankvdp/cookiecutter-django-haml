5e3e4b2523eeb09cd56ee3226a52f384b7341b3c
Frank <frank@plumos.nl>
2013-09-23 15:09:46 +0200

    Adding staticfiles_urlpatterns
    To make sure Local serves statics

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -32,3 +33,4 @@ urlpatterns = patterns('',
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += staticfiles_urlpatterns()
